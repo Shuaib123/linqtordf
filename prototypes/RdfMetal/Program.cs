1d56083baa5f57537abb5743982ce8725727d1bb
matthews.andrew <matthews.andrew@0aa08b0e-b330-0410-a7dc-93bad6e629e8>
2008-06-02 10:34:06 +0000

    initial import

---
+++ prototypes/RdfMetal/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Mono;
+using Mono.GetOptions;
+using SemWeb;
+using SemWeb.Query;
+using SemWeb.Remote;
+
+namespace RdfMetal
+{
+
+    internal class Program
+    {
+        #region queries
+        static string sqGetClasses =
+            @"
+PREFIX owl:  <http://www.w3.org/2002/07/owl#>
+
+SELECT ?u
+WHERE
+	{
+	?u a owl:Class .
+	}
+";
+        static string sqGetProperties =
+            @"
+PREFIX owl:  <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+SELECT DISTINCT ?s
+WHERE
+{
+?s rdfs:domain <{0}>.
+}";
+        static string sqDescribeClass = @"DESCRIBE {0}";
+        #endregion
+
+        private static void Main(string[] args)
+        {
+            Opts opts = ProcessOptions(args);
+            QueryClasses(opts);
+            //QueryProperties(opts);
+        }
+
+        private static IEnumerable<string> GetClassUris(Opts opts)
+        {
+            var source = new SparqlHttpSource(opts.endpoint);
+            var properties = new ClassQuerySink(opts.ignoreBnodes, opts.@namespace, "u");
+            source.RunSparqlQuery(sqGetClasses, properties);
+            return properties.ClassUris.Map(u => u.AbsoluteUri);
+        }
+
+        private static IEnumerable<string> GetClassPropertyUris(Opts opts, string classUri)
+        {
+            var source = new SparqlHttpSource(opts.endpoint);
+            var properties = new ClassQuerySink(opts.ignoreBnodes, opts.@namespace, "s");
+            #region x
+
+            var x1 =
+                @"
+PREFIX owl:  <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+SELECT DISTINCT ?s
+WHERE
+{
+?s rdfs:domain <";
+            var x2 = @">.
+}";
+            #endregion
+            source.RunSparqlQuery(x1 + classUri + x2 , properties);
+            return properties.ClassUris.Map(u => u.AbsoluteUri);
+        }
+
+        private static void QueryClasses(Opts opts)
+        {
+            foreach (var uri in GetClassUris(opts).Distinct())
+            {
+                Debug.WriteLine("Class " + uri);
+                foreach (var propertyUri in GetClassPropertyUris(opts, uri))
+                {
+                    Debug.WriteLine("  Prop: " + propertyUri);
+                }
+            }
+        }
+
+        public static void QueryProperties(Opts opts)
+        {
+            var source = new SparqlHttpSource(opts.endpoint);
+            var properties = new ClassQuerySink(opts.ignoreBnodes, opts.@namespace, "p");
+            source.RunSparqlQuery(sqGetProperties, properties);
+            foreach (Uri uri in properties.ClassUris)
+            {
+                Debug.WriteLine(uri.AbsoluteUri);
+            }
+        }
+
+        private static Opts ProcessOptions(string[] args)
+        {
+            Opts opts = new Opts();
+            opts.ProcessArgs(args);
+            return opts;
+        }
+
+        #region Nested type: Opts
+
+        public class Opts : Options
+        {
+            [Option("The SPARQL endpoint to query.", 'e', "endpoint")]
+            public string endpoint = "http://DBpedia.org/sparql";
+
+            [Option("An XML Namespace to extract classes from.", 'n', "namespace")]
+            public string @namespace = "";
+
+            [Option("Ignore BNodes.", 'i', "ignorebnodes")]
+            public bool ignoreBnodes = false;
+
+            public Opts()
+            {
+                base.ParsingMode = OptionsParsingMode.Both;
+            }
+
+        }
+
+        #endregion
+    }
+
+    public class ClassQuerySink : QueryResultSink
+    {
+        private readonly bool ignoreBnodes;
+        private readonly string @namespace;
+        private readonly string varName;
+
+        public ClassQuerySink(bool ignoreBnodes, string @namespace, string varName)
+        {
+            this.ignoreBnodes = ignoreBnodes;
+            this.@namespace = @namespace;
+            this.varName = varName;
+        }
+
+        public List<Uri> ClassUris = new List<Uri>();
+
+        public override bool Add(VariableBindings result)
+        {
+            Resource resource = result[varName];
+            if (!string.IsNullOrEmpty(resource.Uri))
+            {
+                if (string.IsNullOrEmpty(@namespace) || resource.Uri.StartsWith(@namespace))
+                    ClassUris.Add(new Uri(resource.Uri));
+            }
+            else if (resource is BNode && !ignoreBnodes)
+            {
+                var bn = resource as BNode;
+                if (string.IsNullOrEmpty(@namespace) || bn.LocalName.StartsWith(@namespace))
+                    ClassUris.Add(new Uri(bn.LocalName));
+            }
+            return true;
+        }
+    }
+    public class ClassDetailQuerySink : StatementSink
+    {
+        public ClassDetailQuerySink()
+        {
+        }
+
+        public List<Uri> ls = new List<Uri>();
+
+        public bool Add(Statement s)
+        {
+            string output = string.Format("{0} {1} {2}", s.Subject, s.Predicate, s.Object);
+            Debug.WriteLine(output);
+            return true;
+        }
+    }
+
+    public enum OwlElementType
+    {
+        OwlClass,
+        OwlDatatypeProperty,
+        OwlObjectProperty
+    }
+
+    public static class FunctionalExtensions
+    {
+        public static IEnumerable<R> Map<T, R>(this IEnumerable<T> seq, Func<T, R> f)
+        {
+            foreach (T a in seq)
+            {
+                yield return f(a);
+            }
+        }
+
+
+    }
+}
---
+++ prototypes/RdfMetal/Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Mono;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RdfMetal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Readify Pty Ltd")]
+[assembly: AssemblyProduct("RdfMetal")]
+[assembly: AssemblyCopyright("Copyright © Readify Pty Ltd 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("08fa4e28-7239-4ee3-9208-0bfc73ddcbe1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: UsageComplement("use like this")]
+// This is text that goes after " [options]" in help output.
+
+// Attributes visible in " -V"
+[assembly: Mono.About("Insert About Text Here.")]
+[assembly: Mono.Author("Your name here")]
